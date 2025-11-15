a595f4c90a0c15d981790ed7eda8375a5bafcab1
Kendall Calderon Acuña <calderonacunakendall@gmail.com>
2025-11-14 23:18:14 -0600

    add constraints
    add constraint in all tables

--- LUBRICENTRO_SQL_SCRIPT.sql
+++ LUBRICENTRO_SQL_SCRIPT.sql
@@ -4,6 +4,7 @@ CREATE DATABASE LUBRICENTRO_LOS_ANGELES;
 
 USE LUBRICENTRO_LOS_ANGELES;
 
+
 ------------------ TIPO CLIENTE ------------------
 
 CREATE TABLE TIPO_CLIENTE(
@@ -19,7 +20,7 @@ nombre VARCHAR(20) NOT NULL,
 apellido_1 VARCHAR(20) NOT NULL,
 apellido_2 VARCHAR(20) NOT NULL,
 CONSTRAINT PK_CLIENTE PRIMARY KEY (id_cliente),
-CONSTRAINT FK_TIPO_CLIENTE FOREIGN KEY (id_tipo_cliente) REFERENCES TIPO_CLIENTE (id_tipo_cliente)
+CONSTRAINT FK_TIPO_CLIENTE_CLIENTE FOREIGN KEY (id_tipo_cliente) REFERENCES TIPO_CLIENTE (id_tipo_cliente)
 );
 
 ---TELEFONOS DE LOS CLIENTES
@@ -35,7 +36,7 @@ id_Telefono_Cliente INT NOT NULL,
 id_Cliente INT NOT NULL,
 CONSTRAINT PK_TELEFONO_CLIENTE_CLIENTE PRIMARY KEY(id_Telefono_Cliente_Cliente),
 CONSTRAINT FK_TELEFONOS_CLIENTE FOREIGN KEY (id_Telefono_Cliente) REFERENCES TELEFONOS_CLIENTE(id_telefono_cliente),
-CONSTRAINT FK_CLIENTE FOREIGN KEY (id_Cliente) REFERENCES CLIENTE(id_cliente)
+CONSTRAINT FK_CLIENTE_TELEFONO FOREIGN KEY (id_Cliente) REFERENCES CLIENTE(id_cliente)
 );
 
 ---TABLA DE CORREO ELECTRONICO
@@ -50,7 +51,7 @@ id_correos_cliente_cliente INT IDENTITY (1,1) ,
 id_cliente INT NOT NULL,
 id_correos_cliente INT NOT NULL,
 CONSTRAINT PK_CORREOS_CLIENTES_CLIENTES PRIMARY KEY(id_correos_cliente_cliente),
-CONSTRAINT FK_CLIENTE FOREIGN KEY(id_cliente) REFERENCES CLIENTE (id_cliente),
+CONSTRAINT FK_CLIENTE_CORREOS FOREIGN KEY(id_cliente) REFERENCES CLIENTE (id_cliente),
 CONSTRAINT FK_CORREOS_CLIENTES FOREIGN KEY(id_correos_cliente) REFERENCES CORREOS_CLIENTES (id_correos_cliente)
 );
 
@@ -94,100 +95,111 @@ CONSTRAINT FK_CORREOS_CLIENTES FOREIGN KEY(id_correos_cliente) REFERENCES CORREO
  ------------------ TABLA EMAIL DE TRABAJADORES
 
  CREATE TABLE EMAIL_TRABAJADORES(
-  id_email_trabajador INT IDENTITY(1,1) PRIMARY KEY,
+  id_email_trabajador INT IDENTITY(1,1) ,
   email VARCHAR(20) NOT NULL,
+  CONSTRAINT PK_EMAIL_TRABAJADORES PRIMARY KEY(id_email_trabajador)
  );
 
  CREATE TABLE EMAIL_TRABAJADORES_TRABAJADORES(
-	id_email_trabajadores_trabajadores INT IDENTITY(1,1) PRIMARY KEY,
+	id_email_trabajadores_trabajadores INT IDENTITY(1,1) ,
 	id_email_trabajador INT NOT NULL,
 	id_trabajador INT NOT NULL,
-	FOREIGN KEY (id_email_trabajador) REFERENCES EMAIL_TRABAJADORES (id_email_trabajador),
-	FOREIGN KEY (id_trabajador) REFERENCES TRABAJADORES (id_trabajador)
+	CONSTRAINT PK_EMAIL_TRABAJADORES_TRABAJADORES PRIMARY KEY(id_email_trabajadores_trabajadores),
+	CONSTRAINT FK_EMAIL_TRABAJADORES  FOREIGN KEY (id_email_trabajador) REFERENCES EMAIL_TRABAJADORES (id_email_trabajador),
+	CONSTRAINT FK_TRABAJADORES FOREIGN KEY (id_trabajador) REFERENCES TRABAJADORES (id_trabajador)
  );
 
 	---------------------------------------------------------------------------
 	------------------ FACTURACION ------------------
 
 	CREATE TABLE FACTURA(
-	id_factura INT IDENTITY (1,1) PRIMARY KEY,
+	id_factura INT IDENTITY (1,1),
 	id_cliente INT NOT NULL, --FK
 	monto_total INT NOT NULL,
 	cantidad INT NOT NULL,
 	precio_unitario DECIMAL(10,2) NOT NULL,
 	estado_pago VARCHAR(10) NOT NULL CHECK (estado_pago IN ('pagado', 'pendiente')),
 	fecha_emision DATE NOT NULL,
-	FOREIGN KEY (id_cliente) REFERENCES CLIENTE (id_cliente)
+	CONSTRAINT PK_FACTURA PRIMARY KEY(id_factura),
+	CONSTRAINT FK_CLIENTE_FACTURA FOREIGN KEY (id_cliente) REFERENCES CLIENTE (id_cliente)
 	);
 
 	CREATE TABLE METODOS_PAGO(
-	id_metodo_pago INT IDENTITY (1,1) PRIMARY KEY,
-	tipo VARCHAR(10) NOT NULL CHECK (tipo IN ('efectivo','tarjeta','sinpe'))
+	id_metodo_pago INT IDENTITY (1,1),
+	tipo VARCHAR(10) NOT NULL CHECK (tipo IN ('efectivo','tarjeta','sinpe')),
+	CONSTRAINT PK_METODOS_PAGO PRIMARY KEY(id_metodo_pago)
 	);
 
 	CREATE TABLE FACTURA_METODOS_PAGO(
-	id_factura_metodos_pago INT IDENTITY (1,1) PRIMARY KEY,
+	id_factura_metodos_pago INT IDENTITY (1,1),
 	id_factura INT NOT NULL, --FK
-	id_metodo_pago INT NOT NULL, --FK
-	FOREIGN KEY (id_factura) REFERENCES FACTURA (id_factura),
-	FOREIGN KEY (id_metodo_pago) REFERENCES METODOS_PAGO (id_metodo_pago)
+	id_metodo_pago INT NOT NULL, --FK,
+	CONSTRAINT PK_FACTURA_METODOS_PAGO  PRIMARY KEY(id_factura_metodos_pago),
+	CONSTRAINT FK_FACTURA_METODOS FOREIGN KEY (id_factura) REFERENCES FACTURA (id_factura),
+	CONSTRAINT FK_METODOS_PAGO FOREIGN KEY (id_metodo_pago) REFERENCES METODOS_PAGO (id_metodo_pago)
 	);
 
  ------------------ PRODUCTO ------------------
 ---------ESCRIBIR TABLAS PRODUCTO, PRODUTO_FACTURA
 
  CREATE TABLE PRODUCTO(
-  codigo_producto INT IDENTITY(1,1) PRIMARY KEY,
-  nombre VARCHAR(20)
+  codigo_producto INT IDENTITY(1,1),
+  nombre VARCHAR(20),
+  CONSTRAINT PK_PRODUCTO PRIMARY KEY(codigo_producto)
  );
 
  CREATE TABLE PRODUCTO_FACTURA(
-  id_producto_factura INT IDENTITY(1,1) PRIMARY KEY,
+  id_producto_factura INT IDENTITY(1,1),
   codigo_producto INT,
   id_factura INT,
-  FOREIGN KEY (codigo_producto) REFERENCES PRODUCTO (codigo_producto),
-  FOREIGN KEY (id_factura) REFERENCES FACTURA (id_factura)
+  CONSTRAINT PK_PRODUCTO_FACTURA PRIMARY KEY(id_producto_factura),
+  CONSTRAINT FK_PRODUCTO_FACT FOREIGN KEY (codigo_producto) REFERENCES PRODUCTO (codigo_producto),
+  CONSTRAINT FK_FACTURA_PRODUCTO FOREIGN KEY (id_factura) REFERENCES FACTURA (id_factura)
  );
-
 
 
 ------------------ PROVEEDOR ------------------
 ---------ESCRIBIR TABLAS PROVEEDOR, TELEFONOS_PROVEEDOR, CORREOS_PROVEEDOR, PROVEEDORES_PRODUCTOS,
 ---------------------------------------------------------------------------
 	 CREATE TABLE PROVEEDORES(
-	  id_proveedor INT IDENTITY(1,1) PRIMARY KEY,
+	  id_proveedor INT IDENTITY(1,1) ,
 	  nombre VARCHAR(20),
-	  email VARCHAR(20)
+	  email VARCHAR(20),
+	   CONSTRAINT PK_PROVEEDORES PRIMARY KEY( id_proveedor)
 	 );
 
 	 CREATE TABLE TELEFONOS_PROVEEDOR(
-	  id_telefonos_proveedor INT IDENTITY(1,1) PRIMARY KEY,
+	  id_telefonos_proveedor INT IDENTITY(1,1),
 	  id_proveedor INT,
-	  FOREIGN KEY (id_proveedor) REFERENCES PROVEEDORES (id_proveedor)
+	  CONSTRAINT PK_TELEFONOS_PROVEEDOR PRIMARY KEY( id_telefonos_proveedor),
+	  CONSTRAINT FK_PROVEEDORES_TELEFONOS FOREIGN KEY (id_proveedor) REFERENCES PROVEEDORES (id_proveedor)
 	 );
 
 	 CREATE TABLE PROVEEDORES_PRODUCTOS(
-	  id_proveedor_producto INT IDENTITY(1,1) PRIMARY KEY,
+	  id_proveedor_producto INT IDENTITY(1,1),
 	  id_proveedor INT,
 	  codigo_producto INT,
-	  FOREIGN KEY (id_proveedor) REFERENCES PROVEEDORES (id_proveedor),
-	  FOREIGN KEY (codigo_producto) REFERENCES PRODUCTO (codigo_producto),
+	  CONSTRAINT PK_PROVEEDORES_PRODUCTOS PRIMARY KEY(id_proveedor_producto),
+	 CONSTRAINT FK_PROVEEDORES FOREIGN KEY (id_proveedor) REFERENCES PROVEEDORES (id_proveedor),
+	 CONSTRAINT FK_PRODUCTO_PROVEEDORRES FOREIGN KEY (codigo_producto) REFERENCES PRODUCTO (codigo_producto),
 	 );
 
 ------------------ VEHICULO ------------------
 
 	CREATE TABLE TIPO_VEHICULO(
-	id_tipo_vehiculo INT IDENTITY (1,1) PRIMARY KEY,
+	id_tipo_vehiculo INT IDENTITY (1,1) ,
 	tipo VARCHAR(20) NOT NULL CHECK (tipo IN ('carro', 'moto')),
+	CONSTRAINT PK_TIPO_VEHICULO PRIMARY KEY(id_tipo_vehiculo)
 	);
 
 	CREATE TABLE FECHA_CAMBIO_ACEITE(
-	id_fecha_cambio_aceite INT IDENTITY (1,1) PRIMARY KEY,
-	fecha DATE NOT NULL
+	id_fecha_cambio_aceite INT IDENTITY (1,1),
+	fecha DATE NOT NULL,
+	CONSTRAINT PK_FECHA_CAMBIO_ACEITE PRIMARY KEY(id_fecha_cambio_aceite)
 	);
 
 	CREATE TABLE VEHICULO(
-	id_vehiculo INT IDENTITY (1,1) PRIMARY KEY,
+	id_vehiculo INT IDENTITY (1,1),
 	id_tipo_vehiculo INT NOT NULL, --FK
 	id_cliente INT NOT NULL, --FK
 	id_fecha_cambio_aceite INT NOT NULL, --FK
@@ -196,42 +208,46 @@ CONSTRAINT FK_CORREOS_CLIENTES FOREIGN KEY(id_correos_cliente) REFERENCES CORREO
 	numero_puertas INT NOT NULL,
 	año INT NOT NULL,
 	marca VARCHAR(20) NOT NULL,
-	FOREIGN KEY (id_tipo_vehiculo) REFERENCES TIPO_VEHICULO (id_tipo_vehiculo),
-	FOREIGN KEY (id_cliente) REFERENCES CLIENTE (id_cliente),
-	FOREIGN KEY (id_fecha_cambio_aceite) REFERENCES FECHA_CAMBIO_ACEITE (id_fecha_cambio_aceite)
+	CONSTRAINT PK_VEHICULO PRIMARY KEY(id_vehiculo),
+	CONSTRAINT FK_TIPO_VEHICULO FOREIGN KEY (id_tipo_vehiculo) REFERENCES TIPO_VEHICULO (id_tipo_vehiculo),
+	CONSTRAINT FK_CLIENTE_VEHICULO FOREIGN KEY (id_cliente) REFERENCES CLIENTE (id_cliente),
+	CONSTRAINT FK_FECHA_CAMBIO_ACEITE FOREIGN KEY (id_fecha_cambio_aceite) REFERENCES FECHA_CAMBIO_ACEITE (id_fecha_cambio_aceite)
 	);
 
 ---------------------------------------------------------------------------
 ------------------ CITA ------------------
 ---------------------------------------------------------------------------
 	CREATE TABLE CITA(
-	 id_cita INT IDENTITY (1,1) PRIMARY KEY,
+	 id_cita INT IDENTITY (1,1),
 	 id_cliente INT,
 	 estado_cita VARCHAR(20),
 	 fecha DATE,
 	 hora TIME,
-	 FOREIGN KEY (id_cliente) REFERENCES CLIENTE (id_cliente)
+	 CONSTRAINT PK_CITA  PRIMARY KEY(id_cita),
+	 CONSTRAINT FK_CLIENTE_CITA FOREIGN KEY (id_cliente) REFERENCES CLIENTE (id_cliente)
 	);
 
  ---------------------------------------------------------------------------
  ------------------ SERVICIO ------------------
-CREATE TABLE SERVICIO(
-id_servicio INT IDENTITY (1,1) PRIMARY KEY,
-id_vehiculo INT NOT NULL, --FK
-id_trabajador INT NOT NULL, --FK
-id_factura INT NOT NULL, --FK
-categoria VARCHAR(20) NOT NULL, --CHECK (categoria IN ('HAY QUE VER QUE CATEGORIAS HABRAN'))
-estado_servicio VARCHAR(20) NOT NULL, --CHECK (estado_servicio IN ('HAY QUE VER QUE estados HABRAN'))
-FOREIGN KEY (id_vehiculo) REFERENCES VEHICULO (id_vehiculo),
-FOREIGN KEY (id_trabajador) REFERENCES TRABAJADORES (id_trabajador),
-FOREIGN KEY (id_factura) REFERENCES FACTURA (id_factura),
-);
+	CREATE TABLE SERVICIO(
+	id_servicio INT IDENTITY (1,1),
+	id_vehiculo INT NOT NULL, --FK
+	id_trabajador INT NOT NULL, --FK
+	id_factura INT NOT NULL, --FK
+	categoria VARCHAR(20) NOT NULL, --CHECK (categoria IN ('HAY QUE VER QUE CATEGORIAS HABRAN'))
+	estado_servicio VARCHAR(20) NOT NULL, --CHECK (estado_servicio IN ('HAY QUE VER QUE estados HABRAN'))
+	CONSTRAINT PK_SERVICIO  PRIMARY KEY(id_servicio),
+	CONSTRAINT FK_VEHICULO_SERVICIO FOREIGN KEY (id_vehiculo) REFERENCES VEHICULO (id_vehiculo),
+	CONSTRAINT FK_TRABAJADORES_SERVICIO FOREIGN KEY (id_trabajador) REFERENCES TRABAJADORES (id_trabajador),
+	CONSTRAINT FK_FACTURA_SERVICIO FOREIGN KEY (id_factura) REFERENCES FACTURA (id_factura),
+	);
 
-CREATE TABLE SERVICIO_FACTURA(
-id_servicio_factura INT IDENTITY (1,1) PRIMARY KEY,
-id_servicio INT NOT NULL, --FK
-id_factura INT NOT NULL, --FK
-FOREIGN KEY (id_servicio) REFERENCES SERVICIO (id_servicio),
-FOREIGN KEY (id_factura) REFERENCES FACTURA (id_factura)
-);
+	CREATE TABLE SERVICIO_FACTURA(
+	id_servicio_factura INT IDENTITY (1,1) ,
+	id_servicio INT NOT NULL, --FK
+	id_factura INT NOT NULL, --FK
+	CONSTRAINT PK_SERVICIO_FACTURA PRIMARY KEY(id_servicio_factura),
+	CONSTRAINT FK_SERVICIO_SERVICIO_FACTURA FOREIGN KEY (id_servicio) REFERENCES SERVICIO (id_servicio),
+	CONSTRAINT FK_FACTURA_SERVICIO_FACTURA FOREIGN KEY (id_factura) REFERENCES FACTURA (id_factura)
+	);
 ---------------------------------------------------------------------------
